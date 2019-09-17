05f16eb7727713f7fc14e3122a6accac76a7730d
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-09-17 22:02:59 +0100

    Exercise 3 Completed

---
+++ Ex3.py
@@ -0,0 +1,31 @@
+# Print statement.
+print('I will now count my chickens:')
+# Print string and calculation result. 
+print('Hens', 25.0 + 30.0 / 6)
+# Print string and calculation result. 
+print('Roosters', 100.0 - 25.0 * 3.0 % 4)
+# Print String.
+print('Now I will count the eggs:')
+# Print calculation result. 
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2 - 1 / 4 + 6)
+# Print String
+print('Is it true that 3 + 2 < 5 - 7?')
+# Print calculation result. 
+print(3.0 + 4.0 < 5.0 - 7.0)
+# Print string and calculation result. 
+print('What is 3 + 2?', 3.0 + 2)
+# Print string and calculation result. 
+print('What is 5 - 7?', 5.0 - 7)
+# Print String
+print('Oh, that''s what it''s False')
+# Print String
+print('How about some more.')
+# Print string and calculation result. 
+print('Is it greater?', 5.0 > -2)
+# Print string and calculation result. 
+print('Is it greater or equal?', 5.0 >= 2)
+# Print string and calculation result. 
+print('Is it less or equal?', 5.0 <= -2)
+
+# Instead of new file, apending addition line to this file. 
+print(5 * 35 + 6 / 6)
