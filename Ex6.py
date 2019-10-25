9fb839eea8c7c00eac9d003c24e9a25ef356b7d1
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-10-25 15:27:48 +0100

    Ex9 Commented

--- Ex6.py
+++ Ex6.py
@@ -2,23 +2,34 @@
 
 types_of_people = 10 
 
+# String with variable inside it. 
 x = f"There are {types_of_people} types of people"
 
+# Two variables: Binary, Do_not. 
 binary = "binary"
 do_not = "don't"
+
+# String containing words and the 2 variables inside the strong, Strings within strings. 
 y =  f"Those who know {binary} and those who {do_not}."
 
+# Print the two variables of strings. 
 print(x)
 print(y)
 
+# String and a variable String within it 
 print(f"I said {x}")
 print(f"I also said: '{y}'")
 
+# Boolean variable. 
 hilarious = False 
+
+# String, containing a space for a variable at the end. 
 joke_evolution = "Isn't that joke so funny?! {}"
+
+# .format string instead of f string, with the result of the boolean inserted. 
 print(joke_evolution.format(hilarious))
 
+# Two variables, then printed. 
 w = "This is the left side of ..."
 e = "a string with a right side."
-
 print(w + e)
--- Ex9.py
+++ Ex9.py
@@ -1,11 +1,15 @@
 # Ex9 Printing, Printing, Printing. 
 
+# String of the days of the week. 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# String of the months of the year, each on a new line. 
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
 
 print("Here are the days: ", days)
 print("Here are the months: ", months)
 
+# Using triple quotes """ """ for a multi line string. 
 print("""There's something going on here. 
         witht he three double quotes
         we'll be able to type as much as we like
