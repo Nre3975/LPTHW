b7ff92e071f482c19f50b91984295c609d7dfcef
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-10-25 15:15:22 +0100

    Ex9 Added

---
+++ Ex9.py
@@ -0,0 +1,13 @@
+# Ex9 Printing, Printing, Printing. 
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""There's something going on here. 
+        witht he three double quotes
+        we'll be able to type as much as we like
+        Even 4 lines if we want, or 5 or 6.""")
+
