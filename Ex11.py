854554a2fd92ce6bd6fe4eaca3f320a016ae89db
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-10-26 18:29:02 +0100

    Ex11 Added

---
+++ Ex11.py
@@ -0,0 +1,13 @@
+# Exercise 11: Asking Questions 
+
+# Get data IN to program. 
+# Most Software: Takes input, Computational work on it, give output. 
+
+print("How old are you?", end= ' ')
+age = input() 
+print("How tall are you>", end= ' ')
+height = input()
+print("How much do you weigh", end = ' ')
+weight = input() 
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
