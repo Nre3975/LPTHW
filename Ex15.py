584e42a28ef57227151e32fe633b2e97719bf8b2
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-10-31 21:29:17 +0000

    Ex15 + Study Drills

--- Ex15.py
+++ Ex15.py
@@ -1,2 +1,26 @@
 # Exercise 15: Reading Files. 
 
+# Import argv from the sys library. 
+from sys import argv 
+
+# Unpack argv into script and filename vars. 
+script, filename = argv # pylint: disable=unbalanced-tuple-unpacking 
+
+#Return file object, store in txt var. 
+txt = open(filename)
+
+print(f"Here's your file: {filename}")
+print(txt.read()) #Txt...Read your read command, no params.  
+txt.close()
+
+print ("Type the file name again:", end=' ')
+# Get input, display the message "> "
+file_again = input("> " ) 
+
+# Open a new file, and print it out with read again. 
+txt_again = open(file_again)
+print(txt_again.read())
+txt_again.close() git 
+
+
+                    
