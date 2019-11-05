94f8e4699ecce25eae93cdc3e4d2c898e6d50474
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-05 19:58:48 +0000

    Ex18 Drills

--- Ex18.py
+++ Ex18.py
@@ -2,7 +2,7 @@
 
 # Def used to state defining function, followed by it's name 
 # then arguments, then a colon. Subsequent lines are indented. 
-def print_two(*args): 
+def print_two(*args): #*args: Take all args into function as a list for unpacking. 
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2 {arg2}")
 
@@ -19,3 +19,11 @@ print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
 print_one("First!")
 print_none()
+
+### Function Checks. 
+# Starts with Def. 
+# Name only has Chars and _
+# Arguments inside () after function name. 
+# Arguments have unique names within the funciton namespace. 
+# Indent lines of code. 
+# 
