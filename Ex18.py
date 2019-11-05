664d5c405bac1f995f57de0eab4ba2c83a422f0e
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-05 19:55:37 +0000

    Ex18 Added

---
+++ Ex18.py
@@ -0,0 +1,21 @@
+# Exercise 18: Names, Variables, Code, Functions. 
+
+# Def used to state defining function, followed by it's name 
+# then arguments, then a colon. Subsequent lines are indented. 
+def print_two(*args): 
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2 {arg2}")
+
+def print_two_again(arg1, arg2): 
+    print(f"arg1: {arg1}, arg2 {arg2}")
+
+def print_one(arg1): 
+    print(f"arg1: {arg1}")
+
+def print_none(): 
+    print("I got nothin'.")
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
